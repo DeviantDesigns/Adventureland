53d1bc483c85ce3d45eab0eebbf39be2e1924548
tayreal <tylergagnon25@yahoo.com>
2014-09-09 21:09:08 -0500

    blan

---
+++ Adventureland/Assets/Script/ShootScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootScript : MonoBehaviour {
+
+		public GameObject player;
+		public GameObject bullet;
+		public float bulletImpulse = 50f;
+		
+		// Use this for initialization
+		void Start () 
+		{
+			
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			if(Input.GetButton ("left"))
+			{
+			Instantiate(bullet, transform.position, transform.rotation);	
+			}
+			if(Input.GetButton ("right"))
+			{
+
+			}
+			if(Input.GetButton ("up"))
+			{
+				
+			}
+			if(Input.GetButton ("down"))
+			{
+				
+			}
+			
+		}
+		
+	}
